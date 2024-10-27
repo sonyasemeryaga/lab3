bd3a387798f508f6051255f1f2c284970b8959de
sonyasemeryaga <semeriaha.sofiia@lll.kpi.ua>
2024-10-27 09:21:45 +0200

    Extracted methods increaseQuality and decreaseQuality
    I extracted methods increaseQuality and decreaseQuality because using these methods enhance readability of the code, reduces the amount of code and allow to follow the One Dot Per Line rule

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,29 +11,21 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.itemSellInQuality.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.itemSellInQuality.quality = item.itemSellInQuality.quality - 1;
-                    }
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    decreaseQuality(item.itemSellInQuality);
                 }
             } else {
-                if (item.itemSellInQuality.quality < 50) {
-                    item.itemSellInQuality.quality = item.itemSellInQuality.quality + 1;
+                increaseQuality(item.itemSellInQuality);
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.itemSellInQuality.sellIn < 11) {
-                            if (item.itemSellInQuality.quality < 50) {
-                                item.itemSellInQuality.quality = item.itemSellInQuality.quality + 1;
-                            }
+                            increaseQuality(item.itemSellInQuality);
                         }
 
                         if (item.itemSellInQuality.sellIn < 6) {
-                            if (item.itemSellInQuality.quality < 50) {
-                                item.itemSellInQuality.quality = item.itemSellInQuality.quality + 1;
-                            }
+                            increaseQuality(item.itemSellInQuality);
                         }
                     }
-                }
             }
 
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -43,20 +35,28 @@ class GildedRose {
             if (item.itemSellInQuality.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.itemSellInQuality.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.itemSellInQuality.quality = item.itemSellInQuality.quality - 1;
-                            }
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            decreaseQuality(item.itemSellInQuality);
                         }
                     } else {
                         item.itemSellInQuality.quality = 0;
                     }
                 } else {
-                    if (item.itemSellInQuality.quality < 50) {
-                        item.itemSellInQuality.quality = item.itemSellInQuality.quality + 1;
-                    }
+                    increaseQuality(item.itemSellInQuality);
                 }
             }
+        }
+    }
+
+    private void increaseQuality(ItemSellInQuality itemSellInQuality) {
+        if (itemSellInQuality.quality < 50) {
+            itemSellInQuality.quality += 1;
+        }
+    }
+
+    private void decreaseQuality(ItemSellInQuality itemSellInQuality) {
+        if (itemSellInQuality.quality > 0) {
+            itemSellInQuality.quality -= 1;
         }
     }
 }
