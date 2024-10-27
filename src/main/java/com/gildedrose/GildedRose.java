e8b544f68a9df5379eb53450596c55a8afaeda5f
sonyasemeryaga <semeriaha.sofiia@lll.kpi.ua>
2024-10-27 20:34:15 +0200

    Updated GildedRose.java
    I updated GildedRose.java because of the previous changes in the code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,15 +1,7 @@
 package com.gildedrose;
 
-import java.util.List;
-
 class GildedRose {
     Item[] items;
-
-    private static final List<String> SPECIAL_ITEMS = List.of(
-            "Aged Brie",
-            "Backstage passes to a TAFKAL80ETC concert",
-            "Sulfuras, Hand of Ragnaros"
-    );
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -17,47 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateItem(item);
+            item.updateItem();
         }
-    }
-
-    private void updateItem(Item item) {
-        String name = item.name;
-        if (isSpecialItem(name)) {
-            updateSpecialItem(item.itemSellInQuality, name);
-            return;
-        }
-        updateUsualItems(item.itemSellInQuality);
-    }
-
-    private void updateSpecialItem(ItemSellInQuality itemSellInQuality, String name) {
-        updateBackstagePasses(itemSellInQuality, name);
-        updateAgedBrie(itemSellInQuality, name);
-    }
-
-    private void updateUsualItems(ItemSellInQuality itemSellInQuality) {
-        decreaseQuality(itemSellInQuality);
-        decreaseSellInForNotSulfuras(itemSellInQuality);
-        if (itemSellInQuality.sellIn < 0) decreaseQuality(itemSellInQuality);
-    }
-
-    private void updateAgedBrie(ItemSellInQuality itemSellInQuality, String name) {
-        if (!name.equals("Aged Brie")) return;
-        increaseQuality(itemSellInQuality);
-        decreaseSellInForNotSulfuras(itemSellInQuality);
-        if (itemSellInQuality.sellIn < 0) increaseQuality(itemSellInQuality);
-    }
-
-    private void updateBackstagePasses(ItemSellInQuality itemSellInQuality, String name) {
-        if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) return;
-        increaseQuality(itemSellInQuality);
-        decreaseSellInForNotSulfuras(itemSellInQuality);
-        if (itemSellInQuality.sellIn < 11) increaseQuality(itemSellInQuality);
-        if (itemSellInQuality.sellIn < 6) increaseQuality(itemSellInQuality);
-        if (itemSellInQuality.sellIn < 0) zeroQuality(itemSellInQuality);
-    }
-
-    private boolean isSpecialItem(String name) {
-        return SPECIAL_ITEMS.contains(name);
     }
 }
